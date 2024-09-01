627a48ff2640291678effdfdaa8d4bd7e4a4312d
So-Cyoki <146906786+So-Cyoki@users.noreply.github.com>
2024-09-01 23:58:00 +0900

    忘记修改名字了，改一下

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"Alien_Warplane_3D");
+	win->CreateGameWindow(L"宇宙戦機(Alien_Warplane_3D)");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
